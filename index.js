3d485302dd684eb2b69fbbadb2ab408f77b606bf
Ruben Suarez Alvarez <rubensa@gmail.com>
2019-09-12 07:52:29 +0200

    Enable Video Call
    The UserAgent with this change is: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Edge/18.17763 Safari/537.36

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = Franz => class MicrosoftTeams extends Franz {
   overrideUserAgent() {
-    return window.navigator.userAgent.replace(/(Franz|Electron)([^\s]+\s)/g, '').replace(/(Chrome\/)([^ ]*)/g, '$163.0.3239.84');
+    return window.navigator.userAgent.replace(/(Franz|Electron)([^\s]+\s)/g, '').replace(/(Chrome\/)([^ ]*)/g, 'Chrome/64.0.3282.140 Edge/18.17763');
   }
 };
